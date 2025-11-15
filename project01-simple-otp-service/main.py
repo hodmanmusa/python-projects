2f067333cd7044ef08fd9175df0f338f802daa96
Musa Hodman <hodman.m2015@gmail.com>
2025-11-15 09:09:39 +0300

    feat: basic CLI send-and-validate with attempt limit

--- project01-simple-otp-service/main.py
+++ project01-simple-otp-service/main.py
@@ -8,4 +8,25 @@ def otp_verification ():
     print("\nAn OTP has been sent to your device")
     print(f"OTP sent: {otp}")
 
+    max_attempts = 3
+
+    while max_attempts > 0: 
+        max_attempts -= 1
+
+        try: 
+            input_otp = input("Enter OTP: ")
+            int_input = int(input_otp)
+
+        except ValueError as e: 
+            print(f"Invalid input. Only numbers allowed.", end= " ")    
+        
+        if input_otp != otp and max_attempts>0: 
+            print(f"Incorrect. Attempts left: {max_attempts}")
+        elif input_otp == otp: 
+            print("Verification successful.")
+            return 
+        else: 
+            print("OTP expired. You reached the max attempts limit")
+
+
 otp_verification()
