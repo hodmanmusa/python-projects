b47ed1325318e18cb1ebe5eb9e740971f5f709a5
Musa Hodman <hodman.m2015@gmail.com>
2025-11-15 22:21:39 +0300

    fix: input validation and resend support

--- project01-simple-otp-service/main.py
+++ project01-simple-otp-service/main.py
@@ -6,27 +6,40 @@ def otp_verification ():
 
     print("---- OTP Verification ----")
     print("\nAn OTP has been sent to your device")
-    print(f"OTP sent: {otp}")
+    print(f"OTP sent: {otp}\n")
 
+    input_validator(otp)
+
+
+def input_validator(otp):
+   
     max_attempts = 3
 
     while max_attempts > 0: 
         max_attempts -= 1
-
-        try: 
-            input_otp = input("Enter OTP: ")
-            int_input = int(input_otp)
-
-        except ValueError as e: 
-            print(f"Invalid input. Only numbers allowed.", end= " ")    
         
-        if input_otp != otp and max_attempts>0: 
-            print(f"Incorrect. Attempts left: {max_attempts}")
-        elif input_otp == otp: 
-            print("Verification successful.")
+        try: 
+            user_otp = input("Enter OTP: ")
+            int(user_otp)
+            if len(user_otp) != 6: 
+                raise Exception(f"Invalid input: Please enter an integer of 6 digits. Attempts left: {max_attempts}")
+        
+        except ValueError as te: 
+            print(f"Value Error: The input should be an Integer. Attempts left: {max_attempts}")
+
+        except Exception as e: 
+            print(e)
+        
+        if user_otp == otp: 
+            print("Validated Successfuly. ")
             return 
         else: 
-            print("OTP expired. You reached the max attempts limit")
-
-
+            print(f"The entered OTP is not correct. Please try again. Attempts left{max_attempts}")
+        
+        if max_attempts == 0: 
+            code = input("To resend a new OTP type 'resend': ")
+            if code.lower() == 'resend': 
+                otp_verification()
+            return 
+            
 otp_verification()
