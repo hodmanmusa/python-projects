042341fa66ae9426f3198f208b35690130940654
Musa Hodman <hodman.m2015@gmail.com>
2025-11-19 00:34:35 +0300

    feat: input and validate new expense record

--- project02-expense-tracker/logger_config.py
+++ project02-expense-tracker/logger_config.py
@@ -1,6 +1,10 @@
 import logging
 import json 
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+EXPENSE_PATH = os.path.join(BASE_DIR, "expenses.json")
 
 def log_operation(record:dict): 
-    with open("expense_logger.txt", "a") as file: 
+    with open(EXPENSE_PATH, "a") as file: 
         file.write(file.write(json.dumps(record)+"\n"))
--- project02-expense-tracker/main.py
+++ project02-expense-tracker/main.py
@@ -15,6 +15,12 @@ def start():
         if not is_selection_valid(selection): 
             break 
         
+        if int(selection) == 1: 
+            expense = add_expense()
+            if validate_expense(expense): 
+                save_expense(expense)
+
+        
 
 def display_menu(): 
     options = ['Add expense', 'View all expenses', 'Search expenses', 'Edit expense', 'Delete expense', 'Analysis & Aggregations', 'Undo last action', 'Exit']
@@ -40,6 +46,37 @@ def is_selection_valid(selection):
         print(e)
         return False 
     return True 
+
+def add_expense(): 
+    print("Add expense by providing the details below. ")
+    amount = input("Amount: ")
+    date = input("Date: ")
+    category = input("Category: ")
+    description = input("Description: ")
+    return (amount, date, category, description)
+
+def validate_expense(expense): 
+    amount, date, *others = expense
+    try: 
+        if not amount.isdigit(): 
+            raise ValueError("The amount should be number.")
+        
+        if not datetime.strptime(date, "%Y-%m-%d"): 
+            raise TypeError("Date should be in format YYYY-MM-DD.") 
+        
+    except TypeError as te: 
+        print(te)
+        return False 
+    except ValueError as ve: 
+        print(ve)
+        return False 
+    except Exception as e:
+        print(e)
+    return True 
+
+def save_expense(expense): 
+    amount, date, category, description = expense
+    expense_dict = {"amount": amount, "date": date,"category":category, "description":description}
 
 
 if __name__ == "__main__": 
